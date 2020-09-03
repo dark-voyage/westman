43f36099c05846f32ba64b2d444dfabca87d9d5b
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-03 14:05:57 +0500

    deleted exposed datas

--- layouts/keyboards.js
+++ layouts/keyboards.js
@@ -5,94 +5,8 @@ exports.start = Markup.inlineKeyboard([
 ]);
 
 exports.help = Markup.inlineKeyboard([
-  [Markup.switchToCurrentChatButton("Inline Mode (Beta)", "")],
+  [Markup.urlButton("Westman's Channel", "https://t.me/westmans")],
 ]);
-
-exports.minecraft = Markup.inlineKeyboard([
-  [Markup.callbackButton("🔃 Refresh", "minecraft")],
-  [Markup.urlButton("🌐 Website", "https://bsba.aternos.me/")],
-]);
-
-exports.admin_view = (data) =>
-  Markup.inlineKeyboard([
-    [Markup.urlButton(`Website`, data["profile"])],
-    [
-      Markup.urlButton(`Github`, `https://github.com/${data["github"]}`),
-      Markup.urlButton(`Telegram`, `https://t.me/${data["telegram"]}`),
-    ],
-    [Markup.callbackButton(`⬅ Back`, `admins`)],
-  ]);
-
-exports.admin_list = async (data) => {
-  const list = [];
-  for (let admin of data) {
-    list.push([Markup.callbackButton(admin, `admin_${admin}`)]);
-  }
-  return Markup.inlineKeyboard(list);
-};
-
-exports.check = Markup.inlineKeyboard([
-  Markup.callbackButton(`🔃 Refresh`, `check`),
-]);
-
-exports.form_accept = Markup.inlineKeyboard([
-  [Markup.urlButton(`Community Group`, `https://t.me/chiseldevs`)],
-]);
-
-exports.form_decline = Markup.inlineKeyboard([
-  [Markup.urlButton(`Feedback Zone`, `https://t.me/chiseldevs`)],
-]);
-
-exports.form_complete = Markup.inlineKeyboard([
-  [Markup.callbackButton(`Completed...`, `respond_complete`)],
-  [
-    Markup.urlButton(
-      `Check pending invitations`,
-      `https://github.com/orgs/bsba-team/people/pending_invitations`
-    ),
-  ],
-  [
-    Markup.urlButton(
-      `Check pending collaborators`,
-      `https://github.com/orgs/bsba-team/pending_collaborators`
-    ),
-  ],
-]);
-
-exports.form_request = Markup.inlineKeyboard([
-  [
-    Markup.urlButton(
-      `Check pending invitations`,
-      `https://github.com/orgs/bsba-team/people/pending_invitations`
-    ),
-  ],
-  [
-    Markup.urlButton(
-      `Check pending collaborators`,
-      `https://github.com/orgs/bsba-team/pending_collaborators`
-    ),
-  ],
-]);
-
-exports.form_admin = (data) =>
-  Markup.inlineKeyboard([
-    [
-      Markup.callbackButton(`✅`, `accept_form_${data}`),
-      Markup.callbackButton(`❌`, `decline_form_${data}`),
-    ],
-    [
-      Markup.urlButton(
-        `Check pending invitations`,
-        `https://github.com/orgs/bsba-team/people/pending_invitations`
-      ),
-    ],
-    [
-      Markup.urlButton(
-        `Check pending collaborators`,
-        `https://github.com/orgs/bsba-team/pending_collaborators`
-      ),
-    ],
-  ]);
 
 exports.invalid = Markup.inlineKeyboard([
   Markup.callbackButton(`Show available commands`, `help`),
@@ -115,19 +29,6 @@ exports.audio = Markup.inlineKeyboard([
   ],
 ]);
 
-exports.inline = (data) =>
-  Markup.inlineKeyboard(
-    [
-      Markup.urlButton(`GitHub`, `${data["html_url"]}`),
-      Markup.urlButton(
-        `Download`,
-        `https://github.com/${data["full_name"]}/archive/master.zip`
-      ),
-      Markup.switchToCurrentChatButton(`Repositories`, ``),
-    ],
-    { columns: 2 }
-  );
-
 exports.error_admin = Markup.inlineKeyboard([
   Markup.urlButton(`Contact with admin`, `https://t.me/genemator`),
 ]);
@@ -141,11 +42,3 @@ exports.links = async (links) => {
   }
   return Markup.inlineKeyboard(keyboard);
 };
-
-exports.stream = Markup.inlineKeyboard([
-  [Markup.urlButton(`Genemator`, `http://twitch.com/genemators`)],
-]);
-
-exports.minecraft_account = Markup.inlineKeyboard([
-  Markup.urlButton(`Authorize`, `https://aternos.org/go/`),
-]);
