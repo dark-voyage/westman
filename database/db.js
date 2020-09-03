4faf64673c45aa4b3f42c5be40a49d5586dd19c5
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-03 06:07:34 +0500

    updating formatting

--- database/db.js
+++ database/db.js
@@ -1,4 +1,3 @@
 exports.admins = require("./json/admins.json");
 exports.users = require("./json/users.json");
-exports.teams = require("./json/teams.json");
 exports.links = require("./json/links.json");
