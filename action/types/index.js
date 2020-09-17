d1da573cdfbb84ff6c5922a5f0d40a7528bc71ee
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-18 03:04:53 +0500

    formatting whole project

--- action/types/index.js
+++ action/types/index.js
@@ -1,3 +1,4 @@
+require("./text");
 require("./photo");
 require("./video");
 require("./audio");
--- action/types/text.js
+++ action/types/text.js
@@ -6,36 +6,36 @@ const gifs = require("../../database/db").gifs;
 const counter = require("../../database/counter");
 
 composer.on("text", async (ctx) => {
-    const confessionText = ctx.match[1] // filter.clean(ctx.match[1]);
-    await ctx.telegram.sendMessage(
-        env.CONFESSION,
-        `#message => ${await counter()}` + `\n` + `<i>${confessionText}</i>`,
-        {
-            parse_mode: "HTML",
-        }
-    );
-    await ctx.replyWithHTML(
-        `<b>Thank you for your message. Stay stunned for new updates!</b>`
-    );
+  const confessionText = ctx.match[1]; // filter.clean(ctx.match[1]);
+  await ctx.telegram.sendMessage(
+    env.CONFESSION,
+    `#message => ${await counter()}` + `\n` + `<i>${confessionText}</i>`,
+    {
+      parse_mode: "HTML",
+    }
+  );
+  await ctx.replyWithHTML(
+    `<b>Thank you for your message. Stay stunned for new updates!</b>`
+  );
 });
 
 composer.command("text", async (ctx) => {
-    await ctx.replyWithAnimation(
-        { url: gifs.cm },
-        {
-            parse_mode: "HTML",
-            caption:
-                `<b>You requested text command where it will guide you about sending message to confession</b>` +
-                `\n` +
-                `\n` +
-                `<i>In order to send a message to the channel, please, just text your message!</i>` +
-                `\n` +
-                `\n` +
-                `<i>Example:</i>` +
-                `\n` +
-                `<code>Hello World!</code>`,
-        }
-    );
+  await ctx.replyWithAnimation(
+    { url: gifs.cm },
+    {
+      parse_mode: "HTML",
+      caption:
+        `<b>You requested text command where it will guide you about sending message to confession</b>` +
+        `\n` +
+        `\n` +
+        `<i>In order to send a message to the channel, please, just text your message!</i>` +
+        `\n` +
+        `\n` +
+        `<i>Example:</i>` +
+        `\n` +
+        `<code>Hello World!</code>`,
+    }
+  );
 });
 
 middleware(composer);
