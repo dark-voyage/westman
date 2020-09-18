2a27dcb84627cc7ee879979cebd89e4c45ad7c52
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-18 08:51:24 +0500

    Update help.js

--- action/actions/help.js
+++ action/actions/help.js
@@ -1,6 +1,5 @@
 const { composer, middleware } = require("../../core/bot");
 
-const gifs = require("../../database/db").gifs;
 const consoles = require("../../layouts/consoles");
 const message = require("../../layouts/messages");
 const keyboard = require("../../layouts/keyboards");
