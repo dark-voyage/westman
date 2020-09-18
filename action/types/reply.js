5ade84bd8b18e789a5ffca513a02cbfcd0ddd344
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-18 08:58:17 +0500

    Update reply.js

--- action/types/reply.js
+++ action/types/reply.js
@@ -2,7 +2,6 @@ const { composer, middleware, filter } = require("../../core/bot");
 
 const consoles = require("../../layouts/consoles");
 const env = require("../../core/env");
-const gifs = require("../../database/db").gifs;
 const counter = require("../../database/counter");
 
 composer.hears(
@@ -26,26 +25,24 @@ composer.hears(
 );
 
 composer.hears(/\/reply/, async (ctx) => {
-  await ctx.replyWithAnimation(
-    { url: gifs.cm },
+  await ctx.replyWithHTML(
+      `<b>You requested reply command where you can reply to a message from the confession</b>` +
+      `\n` +
+      `\n` +
+      `<i>In order to reply to a message on channel, please use our templates shown below:</i>` +
+      `\n` +
+      `<b>1.</b> <i>Copy the link of the message you are going to reply to. It should be something like that: https://t.me/westmans/xxx</i>` +
+      `\n` +
+      `<b>2.</b> <code>/reply &lt;message link : message&gt;</code>` +
+      `\n` +
+      `\n` +
+      `<i>Example:</i>` +
+      `\n` +
+      `<code>/reply https://t.me/westmans/14 : Hey you!</code>` +
+      `\n` +
+      `<code>/reply 14 : Hey you!</code>`,
     {
-      parse_mode: "HTML",
-      caption:
-        `<b>You requested reply command where you can reply to a message from the confession</b>` +
-        `\n` +
-        `\n` +
-        `<i>In order to reply to a message on channel, please use our templates shown below:</i>` +
-        `\n` +
-        `<b>1.</b> <i>Copy the link of the message you are going to reply to. It should be something like that: https://t.me/westmans/xxx</i>` +
-        `\n` +
-        `<b>2.</b> <code>/reply &lt;message link : message&gt;</code>` +
-        `\n` +
-        `\n` +
-        `<i>Example:</i>` +
-        `\n` +
-        `<code>/reply https://t.me/westmans/14 : Hey you!</code>` +
-        `\n` +
-        `<code>/reply 14 : Hey you!</code>`,
+      parse_mode: "HTML"
     }
   );
 });
