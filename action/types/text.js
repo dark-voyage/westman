3576a8b65784a425f609dd646aafa9fa859e45a8
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-18 08:58:45 +0500

    Update text.js

--- action/types/text.js
+++ action/types/text.js
@@ -2,7 +2,6 @@ const { composer, middleware, filter } = require("../../core/bot");
 
 const consoles = require("../../layouts/consoles");
 const env = require("../../core/env");
-const gifs = require("../../database/db").gifs;
 const counter = require("../../database/counter");
 
 composer.on("text", async (ctx) => {
@@ -20,20 +19,18 @@ composer.on("text", async (ctx) => {
 });
 
 composer.command("text", async (ctx) => {
-  await ctx.replyWithAnimation(
-    { url: gifs.cm },
+  await ctx.replyWithHTML(
+    `<b>You requested text command where it will guide you about sending message to confession</b>` +
+      `\n` +
+      `\n` +
+      `<i>In order to send a message to the channel, please, just text your message!</i>` +
+      `\n` +
+      `\n` +
+      `<i>Example:</i>` +
+      `\n` +
+      `<code>Hello World!</code>`,
     {
       parse_mode: "HTML",
-      caption:
-        `<b>You requested text command where it will guide you about sending message to confession</b>` +
-        `\n` +
-        `\n` +
-        `<i>In order to send a message to the channel, please, just text your message!</i>` +
-        `\n` +
-        `\n` +
-        `<i>Example:</i>` +
-        `\n` +
-        `<code>Hello World!</code>`,
     }
   );
 });
